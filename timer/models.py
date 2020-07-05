3179c578a83585616f947f3a808ddf8051af0ef3
Takos22 <tora.akos@gmail.com>
2020-07-05 14:44:44 +0200

    Add TIme model

--- timer/models.py
+++ timer/models.py
@@ -1,11 +1,24 @@
 from timer import db
 
+from datetime import datetime
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship("Time", backref="cuber", lazy="dynamic")
 
     def __repr__(self):
         return f"<User id={self.id} username={self.username} email={self.email} password_hash=********>"
+
+
+class Time(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    time_ms = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Time id={self.id} user_id={self.user_id} time_ms={self.time_ms} timestamp={self.timestamp}>"
