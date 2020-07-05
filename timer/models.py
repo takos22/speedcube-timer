b8e1ebdab81a65b2f33299b3b90cdbea43306577
Takos22 <tora.akos@gmail.com>
2020-07-05 14:35:33 +0200

    Add User model

--- timer/models.py
+++ timer/models.py
@@ -1,1 +1,11 @@
 from timer import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return f"<User id={self.id} username={self.username} email={self.email} password_hash=********>"
