79b0486153b782c2a94ffa06dd8ed1321458f60b
Takos22 <tora.akos@gmail.com>
2020-07-07 22:33:18 +0200

    Import correction in models.py werkzeug to werkzeug.security

--- timer/models.py
+++ timer/models.py
@@ -1,7 +1,7 @@
 from timer import db, login
 
 from flask_login import UserMixin
-from werkzeug import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 
