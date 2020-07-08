a77b297af4105e8cfc79be26bed8902ffe6abaa4
Takos22 <tora.akos@gmail.com>
2020-07-08 23:03:11 +0200

    Formatting

--- timer/views.py
+++ timer/views.py
@@ -55,5 +55,4 @@ def logout():
 
 @app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, "static"),
-                               "favicon.ico", mimetype="image/icon")
+    return send_from_directory(os.path.join(app.root_path, "static"), "favicon.ico", mimetype="image/icon")
