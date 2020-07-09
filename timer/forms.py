8a6cf5a959f376206996a3f3668456de66d352f2
Takos22 <tora.akos@gmail.com>
2020-07-09 15:30:07 +0200

    Sort imports

--- timer/forms.py
+++ timer/forms.py
@@ -1,7 +1,8 @@
+from timer.models import User
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
-from timer.models import User
 
 
 class LoginForm(FlaskForm):
