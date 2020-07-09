811b08b5545733fa641dc7b8482d9a2f478eea11
Takos22 <tora.akos@gmail.com>
2020-07-09 16:01:22 +0200

    Change os.getenv to os.environ.get

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     load_dotenv()
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///" + os.path.join(basedir, "database.db"))
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///" + os.path.join(basedir, "database.db"))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
